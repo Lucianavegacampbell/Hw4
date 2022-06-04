153ea71842161ba5e5468c1a20fd3c208c37020f
Lucianavegacampbell <luciana.vega@kellogg.northwestern.edu>
2022-06-04 17:41:49 +0000

    change

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,16 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.where({"user_id"=> session["user_id"]})
+   
+  
+
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"],"user_id"=> session["user_id"] })
+   
   end
 
   def new
@@ -16,6 +20,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["place"]["name"]
+    @place["user_id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_161823) do
   create_table "places", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
